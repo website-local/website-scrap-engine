60a43a33b4ba04265584e398a4436e5bd2d2d644
myfreeer <myfreeer@users.noreply.github.com>
2020-06-16 21:28:53 +0800

    life-cycle: skip unprocessable links

---
+++ src/life-cycle/skip-links.ts
@@ -0,0 +1,15 @@
+import {LinkRedirectFunc} from '../pipeline';
+
+export const skipLinks: LinkRedirectFunc = (url: string) : string | void=> {
+  if (url.startsWith('#') ||
+    url.startsWith('data:') ||
+    url.startsWith('mailto:') ||
+    url.startsWith('javascript:') ||
+    url.startsWith('about:') ||
+    url.startsWith('chrome:') ||
+    url.startsWith('news:') ||
+    url.startsWith('irc:')) {
+    return;
+  }
+  return url;
+};
