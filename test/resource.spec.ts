b14cc315191a0da2d272a3e7613687d410e3fbd9
myfreeer <myfreeer@users.noreply.github.com>
2020-07-02 21:12:06 +0800

    resource: test prepareResourceForClone

--- test/resource.spec.ts
+++ test/resource.spec.ts
@@ -1,4 +1,9 @@
-import {createResource, Resource, ResourceType} from '../src/resource';
+import {
+  createResource,
+  prepareResourceForClone,
+  Resource,
+  ResourceType
+} from '../src/resource';
 import path from 'path';
 
 describe('resource', function () {
@@ -84,10 +89,92 @@ describe('resource', function () {
       'http://nodejs.cn/api/#buffer_buffers_and_typedarrays',
       'http://nodejs.cn/api/',
       '/tmp/aaa');
-    expect(resource).toBeTruthy();
-    expect(resource.type).toBe(ResourceType.Html);
-    expect(resource.depth).toBe(1);
     expect(resource.replacePath).toBe('#buffer_buffers_and_typedarrays');
     expect(resource.savePath).toBe(path.normalize('nodejs.cn/api/index.html'));
+  });
+  test('cross-host-link-resource', () => {
+    const resource: Resource = createResource(ResourceType.Html, 1,
+      'http://nodejs.com/api/#buffer_buffers_and_typedarrays',
+      'http://nodejs.cn/api/',
+      '/tmp/aaa');
+    expect(resource.replacePath).toBe(
+      '../../nodejs.com/api/index.html#buffer_buffers_and_typedarrays');
+    expect(resource.savePath).toBe(path.normalize('nodejs.com/api/index.html'));
+  });
+  test('cross-host-link-resource-with-different-protocol', () => {
+    const resource: Resource = createResource(ResourceType.Html, 1,
+      'https://nodejs.com/api/#buffer_buffers_and_typedarrays',
+      'http://nodejs.cn/api/',
+      '/tmp/aaa');
+    expect(resource.replacePath).toBe(
+      '../../nodejs.com/api/index.html#buffer_buffers_and_typedarrays');
+    expect(resource.savePath).toBe(path.normalize('nodejs.com/api/index.html'));
+  });
+  test('cross-host-link-resource-with-no-protocol', () => {
+    const resource: Resource = createResource(ResourceType.Html, 1,
+      '//nodejs.com/api/#buffer_buffers_and_typedarrays',
+      'https://nodejs.cn/api/',
+      '/tmp/aaa');
+    expect(resource.replacePath).toBe(
+      '../../nodejs.com/api/index.html#buffer_buffers_and_typedarrays');
+    expect(resource.savePath).toBe(path.normalize('nodejs.com/api/index.html'));
+  });
+  test('relative-link', () => {
+    const resource: Resource = createResource(ResourceType.Html, 1,
+      '#buffer_buffers_and_typedarrays',
+      'https://nodejs.com/api/',
+      '/tmp/aaa');
+    expect(resource.replacePath).toBe('#buffer_buffers_and_typedarrays');
+    expect(resource.savePath).toBe(path.normalize('nodejs.com/api/index.html'));
+  });
+  test('same-site-absolute-link', () => {
+    const resource: Resource = createResource(ResourceType.Html, 1,
+      '/#buffer_buffers_and_typedarrays',
+      'https://nodejs.com/api/',
+      '/tmp/aaa');
+    expect(resource.replacePath).toBe('../index.html#buffer_buffers_and_typedarrays');
+    expect(resource.savePath).toBe(path.normalize('nodejs.com/index.html'));
+  });
+  test('prepare-resource-for-clone', () => {
+    const resource: Resource = createResource(ResourceType.Html, 1,
+      '/#buffer_buffers_and_typedarrays',
+      'https://nodejs.com/api/',
+      '/tmp/aaa');
+    const expected: Resource = {
+      type: 2,
+      depth: 1,
+      encoding: 'utf8',
+      url: 'https://nodejs.com/#buffer_buffers_and_typedarrays',
+      rawUrl: '/#buffer_buffers_and_typedarrays',
+      downloadLink: 'https://nodejs.com/',
+      refUrl: 'https://nodejs.com/api/',
+      savePath: 'nodejs.com\\index.html',
+      localRoot: '/tmp/aaa',
+      replacePath: '../index.html#buffer_buffers_and_typedarrays',
+      createTimestamp: resource.createTimestamp,
+      body: undefined,
+      meta: {},
+      host: 'nodejs.com'
+    };
+    expect(prepareResourceForClone(resource)).toEqual(expected);
+    resource.body = expected.body = new Uint8Array(12);
+    expect(prepareResourceForClone(resource)).toEqual(expected);
+    resource.body = expected.body = new ArrayBuffer(24);
+    expect(prepareResourceForClone(resource)).toEqual(expected);
+    resource.body = expected.body = Math.random().toString(36);
+    expect(prepareResourceForClone(resource)).toEqual(expected);
+    resource.body = expected.body = Buffer.alloc(7);
+    expect(prepareResourceForClone(resource)).toEqual(expected);
+    resource.meta = {
+      headers: {aaa: 'bbb', ccc: 'ddd'},
+      testObj1: {},
+      testArr1: [1, 3, 5],
+      testStr: ''
+    };
+    expected.meta = {
+      headers: {aaa: 'bbb', ccc: 'ddd'},
+      testStr: ''
+    };
+    expect(prepareResourceForClone(resource)).toEqual(expected);
   });
 });
