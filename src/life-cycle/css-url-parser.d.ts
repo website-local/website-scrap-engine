fc8dc56d119a655f0385a58bf925b30b94b3dd7d
myfreeer <myfreeer@users.noreply.github.com>
2020-07-04 12:53:00 +0800

    css-url-parser: correct module declaration

--- src/life-cycle/css-url-parser.d.ts
+++ src/life-cycle/css-url-parser.d.ts
@@ -1,9 +1,11 @@
+/**
+ * Parse urls from css file
+ * @param cssText
+ * @return It ignores duplicated urls and base64 encoded resources.
+ * If no urls found empty array will be returned.
+ */
+declare function parseCssUrls(cssText: string): string[];
+
 declare module 'css-url-parser' {
-  /**
-   * Parse urls from css file
-   * @param cssText
-   * @return It ignores duplicated urls and base64 encoded resources.
-   * If no urls found empty array will be returned.
-   */
-  export default function parseCssUrls(cssText: string): string[];
+  export = parseCssUrls;
 }
