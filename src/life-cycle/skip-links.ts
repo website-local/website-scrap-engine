ef9a5d6831b4e200c8c0590f1e5433057f4008d3
myfreeer <myfreeer@users.noreply.github.com>
2020-07-05 20:29:00 +0800

    skip-links: refactor and add test

--- src/life-cycle/skip-links.ts
+++ src/life-cycle/skip-links.ts
@@ -1,18 +1,137 @@
 import {LinkRedirectFunc} from '../pipeline';
 
+// subset of https://en.wikipedia.org/wiki/List_of_URI_schemes
+export const unProcessableUriSchemes = [
+  // Official IANA-registered schemes
+  'about',
+  'attachment',
+  'blob',
+  'cap',
+  'chrome',
+  'chrome-extension',
+  'cid',
+  'content',
+  'cvs',
+  'data',
+  'dav',
+  'dns',
+  'drm',
+  'ed2k',
+  'example',
+  'feed',
+  'file',
+  'filesystem',
+  'ftp',
+  'geo',
+  'git',
+  'icon',
+  'im',
+  'imap',
+  'info',
+  'ipn',
+  'ipp',
+  'ipps',
+  'irc',
+  'irc6',
+  'ircs',
+  'jar',
+  'ldap',
+  'ldaps',
+  'magnet',
+  'mailserver',
+  'mailto',
+  'maps',
+  'market',
+  'message',
+  'mid',
+  'mms',
+  'modem',
+  'ms-help',
+  'ms-settings',
+  'mvn',
+  'news',
+  'nfs',
+  'oid',
+  'pkcs11',
+  'platform',
+  'pop',
+  'redis',
+  'rediss',
+  'res',
+  'resource',
+  'rmi',
+  'rsync',
+  'rtmfp',
+  'rtmp',
+  'rtsp',
+  's3',
+  'service',
+  'sftp',
+  'shttp',
+  'sip',
+  'sips',
+  'skype',
+  'smb',
+  'sms',
+  'snews',
+  'snmp',
+  'spotify',
+  'ssh',
+  'steam',
+  'svn',
+  'tag',
+  'tel',
+  'telnet',
+  'tftp',
+  'udp',
+  'unreal',
+  'urn',
+  'view-source',
+  'vnc',
+  'ws',
+  'wss',
+  'xri',
+  // Unofficial but common URI schemes
+  'admin',
+  'app',
+  'javascript',
+  'jdbc',
+  'odbc'
+];
+
+export const fastUnProcessableUriSchemesMap: Record<string, string[]> = (() => {
+  const map: Record<string, string[]> = {};
+  for (let i = 0, item, l = unProcessableUriSchemes.length; i < l; i++) {
+    item = unProcessableUriSchemes[i];
+    if (map[item[0]]) {
+      map[item[0]].push(item);
+    } else {
+      map[item[0]] = [item];
+    }
+  }
+  return map;
+})();
+
+export interface LinkSkipFunc extends LinkRedirectFunc {
+  (url: string): string | void;
+}
+
 /**
  * Skip unprocessable links
  */
-export const skipLinks: LinkRedirectFunc = (url: string): string | void => {
-  if (url.startsWith('#') ||
-    url.startsWith('data:') ||
-    url.startsWith('mailto:') ||
-    url.startsWith('javascript:') ||
-    url.startsWith('about:') ||
-    url.startsWith('chrome:') ||
-    url.startsWith('news:') ||
-    url.startsWith('irc:')) {
+export const skipLinks: LinkSkipFunc = (url: string): string | void => {
+  if (url.startsWith('#')) {
     return;
+  }
+  const unProcessableUriSchemeList = fastUnProcessableUriSchemesMap[url[0]];
+  if (unProcessableUriSchemeList && unProcessableUriSchemeList.length) {
+    for (let i = 0, item, il, l = unProcessableUriSchemeList.length; i < l; i++) {
+      item = unProcessableUriSchemeList[i];
+      il = item.length;
+      if (url.length > il && url.startsWith(item) && url[il] === ':') {
+        return;
+      }
+    }
   }
   return url;
 };
---
+++ test/life-cycle/skip-links.spec.ts
@@ -0,0 +1,51 @@
+import {
+  skipLinks,
+  unProcessableUriSchemes
+} from '../../src/life-cycle/skip-links';
+
+describe('life-cycle/skip-links', function () {
+  test('skip-un-processable-uri-schemes', () => {
+    for (const scheme of unProcessableUriSchemes) {
+      expect(skipLinks(scheme + '://aaa')).toBeFalsy();
+      expect(skipLinks(scheme + ':bbb')).toBeFalsy();
+    }
+  });
+  test('keep-http-url', () => {
+    const httpUrls = [
+      'http://example.com/aaaa?bbb=ccc#ddd',
+      'http://example.com/aaaa',
+      'https://example.com/aaaa?bbb=ccc#ddd',
+      'https://example.com/aaaa?bbb=ccc',
+      'https://example.com/aaaa#eee'
+    ];
+    for (const url of httpUrls) {
+      expect(skipLinks(url)).toBe(url);
+    }
+  });
+  test('skip-hash-url', () => {
+    const hashUrls = [
+      '#ddd',
+      '#sss=111',
+      '#!/src/process',
+      '#######',
+      '#/menu/demo',
+      '#example'
+    ];
+    for (const url of hashUrls) {
+      expect(skipLinks(url)).toBeFalsy();
+    }
+  });
+  test('keep-relative-url', () => {
+    const httpUrls = [
+      '/aaaa?bbb=ccc#ddd',
+      '/aaaa',
+      'aaaa?bbb=ccc#ddd',
+      'aaaa?bbb=ccc',
+      'aaaa#eee',
+      '?bbb=ccc'
+    ];
+    for (const url of httpUrls) {
+      expect(skipLinks(url)).toBe(url);
+    }
+  });
+});
