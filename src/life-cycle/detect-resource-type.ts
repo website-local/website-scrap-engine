97a256469cd8d4905fc482f477474773b5de81d5
myfreeer <myfreeer@users.noreply.github.com>
2020-07-30 19:47:46 +0800

    detect-resource-type: export lowerCaseExtension
    For implementing custom detectResourceType function

--- src/life-cycle/detect-resource-type.ts
+++ src/life-cycle/detect-resource-type.ts
@@ -19,6 +19,36 @@ export const binaryExtension = arrayToMap([
   'psd'
 ], true);
 
+/**
+ * Return the extension of the path or the url,
+ * from the last '.' to end of string in the last portion of the path.
+ * If there is no '.' in the last portion of the path,
+ * then it returns an empty string.
+ *
+ * @see path.extname
+ * @param url the url to evaluate.
+ */
+export function lowerCaseExtension(url: string): string | void {
+  const hashIndex: number = url.lastIndexOf('#');
+  const searchIndex: number = hashIndex === -1 ?
+    url.lastIndexOf('?') :
+    url.lastIndexOf('?', hashIndex);
+  const endIndex: number = searchIndex === -1 ?
+    hashIndex :
+    hashIndex === -1 ? searchIndex : Math.min(searchIndex, hashIndex);
+  const endPath: number = endIndex === -1 ?
+    url.lastIndexOf('/') :
+    url.lastIndexOf('/', endIndex);
+  const lastIndex: number = endIndex === -1 ?
+    url.lastIndexOf('.') :
+    url.lastIndexOf('.', endIndex);
+  if (lastIndex !== -1 && lastIndex > endPath) {
+    return  endIndex === -1 ?
+      url.slice(lastIndex + 1).toLowerCase() :
+      url.slice(lastIndex + 1, endIndex).toLowerCase();
+  }
+}
+
 export function detectResourceType(
   url: string,
   type: ResourceType
@@ -27,23 +57,8 @@ export function detectResourceType(
     return ResourceType.SiteMap;
   }
   if (type === ResourceType.Html) {
-    const hashIndex: number = url.lastIndexOf('#');
-    const searchIndex: number = hashIndex === -1 ?
-      url.lastIndexOf('?') :
-      url.lastIndexOf('?', hashIndex);
-    const endIndex: number = searchIndex === -1 ?
-      hashIndex :
-      hashIndex === -1 ? searchIndex : Math.min(searchIndex, hashIndex);
-    const endPath: number = endIndex === -1 ?
-      url.lastIndexOf('/') :
-      url.lastIndexOf('/', endIndex);
-    const lastIndex: number = endIndex === -1 ?
-      url.lastIndexOf('.') :
-      url.lastIndexOf('.', endIndex);
-    if (lastIndex !== -1 && lastIndex > endPath) {
-      const extension: string = endIndex === -1 ?
-        url.slice(lastIndex + 1).toLowerCase() :
-        url.slice(lastIndex + 1, endIndex).toLowerCase();
+    const extension: string | void = lowerCaseExtension(url);
+    if (extension) {
       if (binaryExtension[extension]) {
         return ResourceType.Binary;
       } else if ('css' === extension) {
