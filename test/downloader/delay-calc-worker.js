8fb98405cab7b04801ca684a6b33c7b9a14dec9b
myfreeer <myfreeer@users.noreply.github.com>
2021-02-11 13:34:10 +0800

    worker-pool: add missing worker for unit test

---
+++ test/downloader/delay-calc-worker.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const {parentPort} = require('worker_threads');
+const sleep = ms => new Promise(r => setTimeout(r, ms | 0));
+
+parentPort?.addListener('message', async (msg) => {
+  const result = msg.body[0] + msg.body[1];
+  await sleep(300);
+  const message = {
+    taskId: msg.taskId,
+    type: 1,
+    body: result,
+    error: isNaN(result) ? new Error('NaN') : undefined
+  };
+  parentPort?.postMessage(message);
+
+});
