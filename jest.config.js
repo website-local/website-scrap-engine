0f0bea4dd4332b8c3a7de31c63b9b6860621db1e
myfreeer <myfreeer@users.noreply.github.com>
2025-11-23 14:15:54 +0800

    test: ignore a ts-jest warning
    https://github.com/website-local/website-scrap-engine/actions/runs/19264040079/job/55075414192

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,11 @@ import {createDefaultEsmPreset} from 'ts-jest';
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  ...createDefaultEsmPreset(),
+  ...createDefaultEsmPreset({
+    diagnostics: {
+      ignoreCodes: [151002],
+    },
+  }),
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
