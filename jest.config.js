e934bc4722bb6c40f58f45fd28efc97d16c0fd09
myfreeer <myfreeer@users.noreply.github.com>
2026-04-04 16:50:19 +0800

    fix(test): fix ts-jest rootDir config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,14 @@ import {createDefaultEsmPreset} from 'ts-jest';
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  ...createDefaultEsmPreset(),
+  ...createDefaultEsmPreset({
+    tsconfig: {
+      rootDir: '.',
+    },
+    diagnostics: {
+      ignoreCodes: [151002]
+    }
+  }),
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
